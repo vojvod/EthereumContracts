a0ca578becf92cddbeb1413db18e1480c7784de6
vojvod <task@z40>
2018-09-19 11:37:09 +0300

    20180919

--- contracts/Proof.sol
+++ contracts/Proof.sol
@@ -2,32 +2,19 @@ pragma solidity ^0.4.24;
 
 contract Proof
 {
-    /*
-
-    A Solidity contract that can prove file ownership without revealing the actual file.
-    It can prove that the file existed at a particular time and finally check for document integrity.
-
-    It achieves proof of ownership by storing the hash of the file and the owner's name as
-    pairs. It achieves proof of existence by storing the hash of the file and the block
-    timestamp as pairs. Finally, storing the hash itself proves the file integrity; that is, if the file
-    was modified, then its hash will change and the contract won't be able to find any such file,
-    therefore proving that the file was modified.
-
-    */
-
     address private _owner;
 
-    struct OwnerDetails
-    {
+    struct OwnerDetails {
         string firstname;
         string lastname;
         string email;
     }
 
-    struct FileDetails
-    {
+    mapping(string => mapping(uint => OwnerDetails)) _owners;
+
+    struct FileDetails {
         uint timestamp;
-        OwnerDetails owner;
+        uint ownerNumbers;
     }
 
     mapping(string => FileDetails) _files;
@@ -50,30 +37,32 @@ contract Proof
         to.transfer(amount);
     }
 
-    // This is used to store the owner of file at the block timestamp
-    function set(string firstname, string lastname, string email, string fileHash) public payable
-    {
+    function setFile(string firstname, string lastname, string email, string fileHash) public payable {
         require(msg.value > 0);
-
-        // There is no proper way to check if a key already exists or not therefore we are checking for default value i.e., all bits are 0
-        if (_files[fileHash].timestamp == 0)
-        {
-
-            _files[fileHash] = FileDetails(block.timestamp, OwnerDetails(firstname, lastname, email));
-            // We are triggering an event so that the frontend of our app knows that the file's existence and ownership details have been stored
+        if (_files[fileHash].timestamp == 0) {
+            _owners[fileHash][0] = OwnerDetails(firstname, lastname, email);
+            _files[fileHash] = FileDetails(block.timestamp, 0);
             emit logFileAddedStatus(true, block.timestamp, firstname, lastname, email, fileHash);
         }
-        else
-        {
-            // This tells to the frontend that file's existence and ownership details couldn't be stored because the file's details had already been stored earlier
+        else {
             emit logFileAddedStatus(false, block.timestamp, firstname, lastname, email, fileHash);
         }
     }
 
-    // This is used to get file information
-    function get(string fileHash) public constant returns (uint timestamp, string firstname, string lastname, string email)
-    {
-        return (_files[fileHash].timestamp, _files[fileHash].owner.firstname, _files[fileHash].owner.lastname, _files[fileHash].owner.email);
+    function addOwner(string firstname, string lastname, string email, string fileHash) public payable {
+        require(msg.value > 0);
+        if (_files[fileHash].timestamp != 0) {
+            _files[fileHash].ownerNumbers++;
+            _owners[fileHash][_files[fileHash].ownerNumbers] = OwnerDetails(firstname, lastname, email);
+        }
+    }
+
+    function getFile(string fileHash) public constant returns (uint timestamp, uint ownerNumbers, string firstname, string lastname, string email) {
+        return (_files[fileHash].timestamp, _files[fileHash].ownerNumbers, _owners[fileHash][0].firstname, _owners[fileHash][0].lastname, _owners[fileHash][0].email);
+    }
+
+    function getFileOwner(string fileHash, uint ownerNumber) public constant returns (string ownerFirstName, string ownerLastName, string ownerEmail) {
+        return (_owners[fileHash][ownerNumber].firstname, _owners[fileHash][ownerNumber].lastname, _owners[fileHash][ownerNumber].email);
     }
 
 }
