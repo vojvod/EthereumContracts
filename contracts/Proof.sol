4d7ec26f7d9b3586bc0b38c7695615d2ec380ae2
vojvod <task@z40>
2018-09-19 12:12:57 +0300

    20180919_1

--- contracts/Proof.sol
+++ contracts/Proof.sol
@@ -15,12 +15,23 @@ contract Proof
     struct FileDetails {
         uint timestamp;
         uint ownerNumbers;
+        address mainOwner;
     }
 
     mapping(string => FileDetails) _files;
 
     modifier validOwner() {
         require(msg.sender == _owner);
+        _;
+    }
+
+    modifier validValue() {
+        require(msg.value > 0);
+        _;
+    }
+
+    modifier validMainFileOwner(string fileHash) {
+        require(msg.sender == _files[fileHash].mainOwner);
         _;
     }
 
@@ -37,11 +48,10 @@ contract Proof
         to.transfer(amount);
     }
 
-    function setFile(string firstname, string lastname, string email, string fileHash) public payable {
-        require(msg.value > 0);
+    function setFile(string firstname, string lastname, string email, string fileHash) validValue public payable {
         if (_files[fileHash].timestamp == 0) {
             _owners[fileHash][0] = OwnerDetails(firstname, lastname, email);
-            _files[fileHash] = FileDetails(block.timestamp, 0);
+            _files[fileHash] = FileDetails(block.timestamp, 0, msg.sender);
             emit logFileAddedStatus(true, block.timestamp, firstname, lastname, email, fileHash);
         }
         else {
@@ -49,8 +59,7 @@ contract Proof
         }
     }
 
-    function addOwner(string firstname, string lastname, string email, string fileHash) public payable {
-        require(msg.value > 0);
+    function addOwner(string firstname, string lastname, string email, string fileHash) validValue validMainFileOwner(fileHash) public payable {
         if (_files[fileHash].timestamp != 0) {
             _files[fileHash].ownerNumbers++;
             _owners[fileHash][_files[fileHash].ownerNumbers] = OwnerDetails(firstname, lastname, email);
