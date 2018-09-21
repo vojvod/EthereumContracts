0fa598d7af44a8b43fe706d34dced284a0c5ae8a
vojvod <task@z40>
2018-09-21 10:54:29 +0300

    20180921

--- gui/src/routes/dashboard.js
+++ gui/src/routes/dashboard.js
@@ -34,55 +34,55 @@ const dashboardRoutes = [
         icon: "pe-7s-delete-user",
         component: Proof
     },
-    {
-        path: "/dashboard",
-        name: "Dashboard",
-        icon: "pe-7s-graph",
-        component: Dashboard
-    },
-    {
-        path: "/user",
-        name: "User Profile",
-        icon: "pe-7s-user",
-        component: UserProfile
-    },
-    {
-        path: "/table",
-        name: "Table List",
-        icon: "pe-7s-note2",
-        component: TableList
-    },
-    {
-        path: "/typography",
-        name: "Typography",
-        icon: "pe-7s-news-paper",
-        component: Typography
-    },
-    {
-        path: "/icons",
-        name: "Icons",
-        icon: "pe-7s-science",
-        component: Icons
-    },
-    {
-        path: "/maps",
-        name: "Maps",
-        icon: "pe-7s-map-marker",
-        component: Maps
-    },
-    {
-        path: "/notifications",
-        name: "Notifications",
-        icon: "pe-7s-bell",
-        component: Notifications
-    },
-    {
-        upgrade: true,
-        path: "/upgrade",
-        name: "Upgrade to PRO",
-        icon: "pe-7s-rocket",
-        component: Upgrade
-    },
+    // {
+    //     path: "/dashboard",
+    //     name: "Dashboard",
+    //     icon: "pe-7s-graph",
+    //     component: Dashboard
+    // },
+    // {
+    //     path: "/user",
+    //     name: "User Profile",
+    //     icon: "pe-7s-user",
+    //     component: UserProfile
+    // },
+    // {
+    //     path: "/table",
+    //     name: "Table List",
+    //     icon: "pe-7s-note2",
+    //     component: TableList
+    // },
+    // {
+    //     path: "/typography",
+    //     name: "Typography",
+    //     icon: "pe-7s-news-paper",
+    //     component: Typography
+    // },
+    // {
+    //     path: "/icons",
+    //     name: "Icons",
+    //     icon: "pe-7s-science",
+    //     component: Icons
+    // },
+    // {
+    //     path: "/maps",
+    //     name: "Maps",
+    //     icon: "pe-7s-map-marker",
+    //     component: Maps
+    // },
+    // {
+    //     path: "/notifications",
+    //     name: "Notifications",
+    //     icon: "pe-7s-bell",
+    //     component: Notifications
+    // },
+    // {
+    //     upgrade: true,
+    //     path: "/upgrade",
+    //     name: "Upgrade to PRO",
+    //     icon: "pe-7s-rocket",
+    //     component: Upgrade
+    // },
     {redirect: true, path: "/", to: "/fileDetails", name: "File Details"}
 ];
 
