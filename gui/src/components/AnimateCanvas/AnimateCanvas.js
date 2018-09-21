9c7e515e8ebd26e820fc11c45a3b1e0829e2ae87
Simeon Taskaris <simos.taskaris@gmail.com>
2018-09-21 23:26:36 +0300

    20180921

--- gui/src/components/AnimateCanvas/AnimateCanvas.js
+++ gui/src/components/AnimateCanvas/AnimateCanvas.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
+import "./AnimateCanvas.css";
 
 class AnimateCanvas extends Component {
-
     render() {
         return (
-            <canvas id={this.props.id} style={{position: "absolute", width: "100%", minHeight: "calc(100% - 143px);"}}></canvas>
+            <canvas className={"canvas-body"} id={this.props.id} style={{
+                position: "absolute",
+                width: "100%",
+                height: "calc(100% - 143px)"
+            }}> </canvas>
         );
     }
 }
