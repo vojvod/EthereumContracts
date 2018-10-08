38d655668eb054dae054acfd011411f900926df1
Simeon Taskaris <simos.taskaris@gmail.com>
2018-10-08 23:49:53 +0300

    run test

--- test/proof.js
+++ test/proof.js
@@ -11,11 +11,13 @@ contract("Proof", accounts => {
     const lastname = 'MyLastName';
     const email = 'Myemail';
     const fileHash = '0xdd870fa1b7c4700f2bd7f44238821c26f7392148';
+    const ipfsHash = "Qmb1E7YpmnQYEqRKcr6WjTFgKubYkWtN4PzdwKp6Tud3TA";
+    const ipfsFileType = "test.pdf";
 
     it('should revert the transaction of setFile if value is 0', () => {
         return Proof.deployed()
             .then(instance => {
-                return instance.setFile(firstname, lastname, email, fileHash, {from: firstOwnerAddress, value: 0});
+                return instance.setFile(firstname, lastname, email, fileHash, ipfsHash, ipfsFileType, {from: firstOwnerAddress, value: 0});
             })
             .then(result => {
                 console.log(result);
