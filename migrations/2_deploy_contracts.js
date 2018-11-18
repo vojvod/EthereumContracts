707fb7ae89ef2fa255dc296b0bd0583924ad6590
Simeon Taskaris <simos.taskaris@gmail.com>
2018-11-18 02:36:10 +0200

    20181118

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,6 +3,7 @@ var ERC20 = artifacts.require("./ERC20.sol");
 var ERC223 = artifacts.require("./ERC223.sol");
 var ERC223ReceivingContract = artifacts.require("./ERC223ReceivingContract.sol");
 var Token = artifacts.require("./Token.sol");
+var SafeMath = artifacts.require("./SafeMath.sol");
 var DevelodioToken = artifacts.require("./DevelodioToken.sol");
 
 module.exports = function(deployer) {
@@ -11,5 +12,6 @@ module.exports = function(deployer) {
     deployer.deploy(ERC223);
     deployer.deploy(ERC223ReceivingContract);
     deployer.deploy(Token);
+    deployer.deploy(SafeMath);
     deployer.deploy(DevelodioToken);
 };
